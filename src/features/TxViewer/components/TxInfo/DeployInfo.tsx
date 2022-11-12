41eae2f80044fc68d5b5355f66182eea4888149e
Kari <evergreenkary@gmail.com>
2022-11-12 12:32:04 +0800

    change info layout

--- src/features/TxViewer/components/TxInfo/DeployInfo.tsx
+++ src/features/TxViewer/components/TxInfo/DeployInfo.tsx
@@ -1,14 +1,11 @@
 import styled from "styled-components";
 
-import { Entry, InfoBox } from "./styled";
-import { MonoFont } from "../../../../components/styled";
-import {
-    DeclareTransactionInfo,
-    DeployTransactionInfo,
-    TransactionReceipt,
-} from "../../../../lib/types/transaction";
+import { AccordionArrayWrapper } from "./styled";
+import { BottomInfo, Entry, InfoBox, InfoContainer } from "./styled";
+import Accordion from "../../../../components/Accordion";
+import { DeployTransactionInfo } from "../../../../lib/types/transaction";
 
-const Container = styled(InfoBox)``;
+const Container = styled(InfoContainer)``;
 
 const Wrapper = styled.div`
     display: flex;
@@ -22,38 +19,54 @@ interface DeployInfoProps {
 function DeployInfo({ info }: DeployInfoProps) {
     return (
         <Container>
-            <Wrapper>
-                <Entry
-                    title="Block hash"
-                    value={info.block_hash || "PENDING"}
-                />
-                <Entry
-                    title="Block number"
-                    value={info.block_number || "PENDING"}
-                />
-                <Entry title="Index" value={info.transaction_index} />
-                <Entry
-                    title="Status"
-                    value={info.status.replaceAll("_", " ")}
-                />
-            </Wrapper>
+            <InfoBox>
+                <Wrapper>
+                    <Entry
+                        title="Block hash"
+                        value={info.block_hash || "PENDING"}
+                    />
+                    <Entry
+                        title="Block number"
+                        value={info.block_number || "PENDING"}
+                    />
+                    <Entry title="Index" value={info.transaction_index} />
+                    <Entry
+                        title="Status"
+                        value={info.status.replaceAll("_", " ")}
+                    />
+                </Wrapper>
 
-            <Wrapper>
-                <Entry title="Class hash" value={info.transaction.class_hash} />
-                <Entry
-                    title="Contract address"
-                    value={info.transaction.contract_address}
-                />
-            </Wrapper>
+                <Wrapper>
+                    <Entry
+                        title="Class hash"
+                        value={info.transaction.class_hash}
+                    />
+                    <Entry
+                        title="Contract address"
+                        value={info.transaction.contract_address}
+                    />
+                </Wrapper>
 
-            <Wrapper>
-                <Entry
-                    title="Contract address salt"
-                    value={info.transaction.contract_address_salt}
-                />
-                <Entry title="Version" value={info.transaction.version} />
-                <Entry title="Timestamp" value="timestamp here" />
-            </Wrapper>
+                <Wrapper>
+                    <Entry
+                        title="Contract address salt"
+                        value={info.transaction.contract_address_salt}
+                    />
+                    <Entry title="Version" value={info.transaction.version} />
+                    <Entry title="Timestamp" value="timestamp here" />
+                </Wrapper>
+            </InfoBox>
+
+            <BottomInfo>
+                <Accordion
+                    title={`Constructor calldata (${info.transaction.constructor_calldata.length})`}
+                    style={{ flex: 1 }}
+                >
+                    <AccordionArrayWrapper
+                        array={info.transaction.constructor_calldata}
+                    />
+                </Accordion>
+            </BottomInfo>
         </Container>
     );
 }
